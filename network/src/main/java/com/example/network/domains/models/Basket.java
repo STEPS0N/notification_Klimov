8cbc7353af432762eba911e4944ec61c2832919c
STEPS0N <stepan.klimoff@mail.ru>
2026-05-11 20:35:52 +0500

    Импортирование библиотеки, настройка проекта. Климов

---
+++ network/src/main/java/com/example/network/domains/models/Basket.java
@@ -0,0 +1,11 @@
+package com.example.network.domains.models;
+
+public class Basket {
+    public Integer idProduct;
+    public Integer count;
+
+    public Basket(Integer idProduct, Integer count) {
+        this.idProduct = idProduct;
+        this.count = count;
+    }
+}
